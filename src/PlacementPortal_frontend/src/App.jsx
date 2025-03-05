6db7c2423d15cf0c66d35ece592970f14587107a
Archana Lakra <127103215+A-lakra@users.noreply.github.com>
2025-03-05 15:20:20 +0530

    Updated App.jsx

--- src/PlacementPortal_frontend/src/App.jsx
+++ src/PlacementPortal_frontend/src/App.jsx
@@ -1,35 +1,5 @@
 
 import { PlacementPortal_backend } from 'declarations/PlacementPortal_backend';
-
-// function App() {
-//   const [greeting, setGreeting] = useState('');
-
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//     const name = event.target.elements.name.value;
-//     PlacementPortal_backend.greet(name).then((greeting) => {
-//       setGreeting(greeting);
-//     });
-//     return false;
-//   }
-
-//   return (
-//     <main>
-//       <img src="/logo2.svg" alt="DFINITY logo" />
-//       <br />
-//       <br />
-//       <form action="#" onSubmit={handleSubmit}>
-//         <label htmlFor="name">Enter your name: &nbsp;</label>
-//         <input id="name" alt="Name" type="text" />
-//         <button type="submit">Click Me!</button>
-//       </form>
-//       <section id="greeting">{greeting}</section>
-//     </main>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
